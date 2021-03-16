a37a537b462229e35f377e47188afbd4a5f31b0c
AnOtterGithubUser <thomaspesneau@gmail.com>
2021-03-16 21:59:54 +0100

    Add Prefect workflow and scheduling

--- workflow.py
+++ workflow.py
@@ -1,28 +1,32 @@
-from prefect import Task, Flow
+from prefect import task, Flow
+from prefect.schedules import IntervalSchedule
 import papermill as pm
 import datetime
 
 
-current_date = datetime.datetime.now()
-date_format = "%Y-%m-%d"
+
+schedule = IntervalSchedule(
+    start_date=datetime.datetime.strptime("2021-03-17-08-00-00", "%Y-%m-%d-%H-%M-%S"),
+    interval=datetime.timedelta(days=1)
+)
 
 
-@task
+@task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def fetch_data():
     pm.execute_notebook("./01_fetch_data.ipynb",
-                        "./01_fetch_data_%s.ipynb" % current_date.strftime(date_format),
+                        "./outputs/01_fetch_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
                         parameters={"BTC_TICKER":"BTC-USD",
                                     "NB_DAYS":0,
                                     "DATE_FORMAT":"%Y-%m-%d",
                                     "DATA_DIR":"data",
                                     "OUTPUT_DIRNAME":"01_raw",
                                     "OUTPUT_FILENAME":"raw_data.csv",
-                                    "EXECUTION_DATE":current_date.strftime(date_format)})
+                                    "EXECUTION_DATE":datetime.datetime.now().strftime("%Y-%m-%d")})
     
 @task
-def transform_data():
+def transform_data(upstream_task):
     pm.execute_notebook("./02_transform_data.ipynb",
-                        "./02_transform_data_%s.ipynb" % current_date.strftime(date_format),
+                        "./outputs/02_transform_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
                         parameters={"BTC_TICKER": "BTC-USD",
                                     "DATA_DIR": "data",
                                     "INPUT_DIRNAME": "01_raw",
@@ -32,12 +36,12 @@ def transform_data():
                                     "DATE_COLUMN": "Date",
                                     "INPUT_FILENAME": "raw_data.csv",
                                     "OUTPUT_FILENAME": "clean_data.csv",
-                                    "EXECUTION_DATE": current_date.strftime(date_format)})
+                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")})
     
 @task
-def predict():
+def predict(upstream_task):
     pm.execute_notebook("./03_predictions.ipynb",
-                        "./03_predictions_%s.ipynb" % current_date.strftime(date_format),
+                        "./outputs/03_predictions_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
                         parameters={"MODEL_PATH": "artifacts/2021-03-14/",
                                     "MODEL_NAME": "LinearRegression",
                                     "MODEL_EXTENSION": ".joblib",
@@ -50,14 +54,14 @@ def predict():
                                     "DAY_PLUS_1": "DAY_PLUS_1",
                                     "DAY_PLUS_7": "DAY_PLUS_7",
                                     "DAY_PLUS_30": "DAY_PLUS_30",
-                                    "EXECUTION_DATE": current_date.strftime(date_format),
+                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d"),
                                     "NB_DAYS": 5})
     
 @task
-def compute_metrics():
+def compute_metrics(upstream_task):
     pm.execute_notebook("./04_metrics.ipynb",
-                        "./04_metrics_%s.ipynb" % current_date.strftime(date_format),
-                        parameters={"EXECUTION_DATE": current_date.strftime(date_format),
+                        "./outputs/04_metrics_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        parameters={"EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d"),
                                     "DATA_DIR": "data",
                                     "INPUT_DIRNAME": "03_predictions",
                                     "INPUT_FILENAME": "predictions.csv",
@@ -69,8 +73,10 @@ def compute_metrics():
                                     "NB_LAST_DAYS": 30})
 
 
-with Flow("btc_pipeline") as flow:
+with Flow("btc_pipeline", schedule=schedule) as flow:
     data = fetch_data()
     transformed_data = transform_data(data)
     predictions = predict(transformed_data)
     metrics = compute_metrics(predictions)
+    
+flow.run()
