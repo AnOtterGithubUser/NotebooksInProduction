9be1aae45116bfa8855406159a6f0d87ee891dbc
AnOtterGithubUser <thomaspesneau@gmail.com>
2021-03-23 22:55:47 +0100

    v0.1.0

--- workflow.py
+++ workflow.py
@@ -2,6 +2,7 @@ from prefect import task, Flow
 from prefect.schedules import IntervalSchedule
 import papermill as pm
 import datetime
+import os
 
 
 
@@ -14,19 +15,20 @@ schedule = IntervalSchedule(
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def fetch_data():
     pm.execute_notebook("./01_fetch_data.ipynb",
-                        "./outputs/01_fetch_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./01_fetch_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"BTC_TICKER":"BTC-USD",
                                     "NB_DAYS":0,
                                     "DATE_FORMAT":"%Y-%m-%d",
                                     "DATA_DIR":"data",
                                     "OUTPUT_DIRNAME":"01_raw",
                                     "OUTPUT_FILENAME":"raw_data.csv",
-                                    "EXECUTION_DATE":datetime.datetime.now().strftime("%Y-%m-%d")})
+                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")
+                                   })
     
 @task
 def transform_data(upstream_task):
     pm.execute_notebook("./02_transform_data.ipynb",
-                        "./outputs/02_transform_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./02_transform_data_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"BTC_TICKER": "BTC-USD",
                                     "DATA_DIR": "data",
                                     "INPUT_DIRNAME": "01_raw",
@@ -36,12 +38,13 @@ def transform_data(upstream_task):
                                     "DATE_COLUMN": "Date",
                                     "INPUT_FILENAME": "raw_data.csv",
                                     "OUTPUT_FILENAME": "clean_data.csv",
-                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")})
+                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")
+                                   })
     
 @task
 def predict_linear_regression(upstream_task):
     pm.execute_notebook("./03_predictions.ipynb",
-                        "./outputs/03_predictions_linear_regression_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./03_predictions_linear_regression_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"MODEL_PATH": "artifacts/2021-03-18/",
                                     "MODEL_NAME": "LinearRegression",
                                     "MODEL_EXTENSION": ".joblib",
@@ -60,7 +63,7 @@ def predict_linear_regression(upstream_task):
 @task
 def predict_gradient_boosting(upstream_task):
     pm.execute_notebook("./03_predictions.ipynb",
-                        "./outputs/03_predictions_gradient_boosting_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./03_predictions_gradient_boosting_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"MODEL_PATH": "artifacts/2021-03-18/",
                                     "MODEL_NAME": "GradientBoostingRegressor",
                                     "MODEL_EXTENSION": ".joblib",
@@ -79,7 +82,7 @@ def predict_gradient_boosting(upstream_task):
 @task
 def predict_prophet(upstream_task):
     pm.execute_notebook("./03_predictions_prophet.ipynb",
-                        "./outputs/03_predictions_prophet_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./03_predictions_prophet_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"MODEL_PATH": "artifacts/2021-03-18/",
                                     "MODEL_NAME": "prophet.json",
                                     "DATA_DIR": "data",
@@ -91,12 +94,13 @@ def predict_prophet(upstream_task):
                                     "DAY_PLUS_1": "DAY_PLUS_1",
                                     "DAY_PLUS_7": "DAY_PLUS_7",
                                     "DAY_PLUS_30": "DAY_PLUS_30",
-                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")})
-    
+                                    "EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d")
+                                   })
+
 @task
 def compute_metrics(**upstream_task):
     pm.execute_notebook("./04_metrics.ipynb",
-                        "./outputs/04_metrics_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                        "./04_metrics_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"),
                         parameters={"EXECUTION_DATE": datetime.datetime.now().strftime("%Y-%m-%d"),
                                     "DATA_DIR": "data",
                                     "INPUT_DIRNAME": "03_predictions",
@@ -108,6 +112,10 @@ def compute_metrics(**upstream_task):
                                     "DAY_PLUS_30": "DAY_PLUS_30",
                                     "NB_LAST_DAYS": 30})
 
+@task
+def display_webapp(metrics):
+    os.system("voila 04_metrics_%s.ipynb" % datetime.datetime.now().strftime("%Y-%m-%d"))
+
 
 with Flow("btc_pipeline", schedule=schedule) as flow:
     data = fetch_data()
@@ -116,5 +124,6 @@ with Flow("btc_pipeline", schedule=schedule) as flow:
     predictions_gradient_boosting = predict_gradient_boosting(transformed_data)
     predictions_prophet = predict_prophet(transformed_data)
     metrics = compute_metrics(pred_lr=predictions_linear_regression, pred_gb=predictions_gradient_boosting, pred_prophet=predictions_prophet)
+    display_webapp(metrics)
     
 flow.run()
